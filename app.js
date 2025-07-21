ac8daa8f7d99bd6c928e01f2d90be75c6696d265
24020804-MargaretPabustan <24020804@myrp.edu.sg>
2025-07-21 16:56:10 +0800

    CA2 Interim First Commit

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const mysql = require('mysql2');
-const session = require('express-session');
-const flash = require('connect-flash');
 const multer = require('multer');
 const app = express();
+
+const connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password: 'Republic_C207',
+database: 'publishers_db'
+});
+
+connection.connect((err) => {
+if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+}
+console.log('Connected to MySQL database');
+});
+
+
 
 // Set up multer for file uploads
 const storage = multer.diskStorage({
@@ -17,302 +32,159 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const connection = mysql.createConnection({
-    host: 'w-mmc8.h.filess.io',
-    port: 3307,
-    user: 'C237database2_polebread',
-    password: '9927aebd694eeec189edeb339eb537defa7ab0f6',
-    database: 'C237database2_polebread'
-});
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
-});
 
 // Set up view engine
 app.set('view engine', 'ejs');
+
 //  enable static files
 app.use(express.static('public'));
+
 // enable form processing
 app.use(express.urlencoded({
     extended: false
 }));
 
-//TO DO: Insert code for Session Middleware below 
-app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
-    // Session expires after 1 week of inactivity
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 } 
-}));
 
-app.use(flash());
-
-// Middleware to check if user is logged in
-const checkAuthenticated = (req, res, next) => {
-    if (req.session.user) {
-        return next();
-    } else {
-        req.flash('error', 'Please log in to view this resource');
-        res.redirect('/login');
-    }
-};
-
-// Middleware to check if user is admin
-const checkAdmin = (req, res, next) => {
-    if (req.session.user.role === 'admin') {
-        return next();
-    } else {
-        req.flash('error', 'Access denied');
-        res.redirect('/shopping');
-    }
-};
-
-// Middleware for form validation
-const validateRegistration = (req, res, next) => {
-    const { username, email, password, address, contact, role } = req.body;
-
-    if (!username || !email || !password || !address || !contact || !role) {
-        return res.status(400).send('All fields are required.');
-    }
-    
-    if (password.length < 6) {
-        req.flash('error', 'Password should be at least 6 or more characters long');
-        req.flash('formData', req.body);
-        return res.redirect('/register');
-    }
-    next();
-};
-
-// Define routes
-app.get('/',  (req, res) => {
-    res.render('index', {user: req.session.user} );
-});
-
-app.get('/inventory', checkAuthenticated, checkAdmin, (req, res) => {
-    // Fetch data from MySQL
-    connection.query('SELECT * FROM products', (error, results) => {
-      if (error) throw error;
-      res.render('inventory', { products: results, user: req.session.user });
-    });
-});
-
-app.get('/register', (req, res) => {
-    res.render('register', { messages: req.flash('error'), formData: req.flash('formData')[0] });
-});
-
-app.post('/register', validateRegistration, (req, res) => {
-
-    const { username, email, password, address, contact, role } = req.body;
-
-    const sql = 'INSERT INTO users (username, email, password, address, contact, role) VALUES (?, ?, SHA1(?), ?, ?, ?)';
-    connection.query(sql, [username, email, password, address, contact, role], (err, result) => {
-        if (err) {
-            throw err;
+//Default route for publisher table//
+app.get('/', (req,res) => {
+    const sql = 'SELECT * FROM publishers';
+    //Fetch data from MySQL
+    connection.query(sql, (error,results) => {
+        if (error) {
+            console.log('Database query error:', error.message);
+            return res.status(500).send('Error Retrieving Publishers');
+            
         }
-        console.log(result);
-        req.flash('success', 'Registration successful! Please log in.');
-        res.redirect('/login');
+        //Render HTML page with data
+        res.render('index', {publishers:results});
     });
 });
+
+
+
+
+//Display details of a particular publisher//
+app.get('/publishers/:id', (req, res) => {
+  // Extract the product ID from the request parameters
+  const publisher_id = req.params.id;
+
+  // Fetch data from MySQL based on the publisher ID
+  connection.query('SELECT * FROM publishers WHERE publisher_id = ?', [publisher_id], (error, results) => {
+      if (error) throw error;
+
+      // Check if any product with the given ID was found
+      if (results.length > 0) {
+          // Render HTML page with the publishers data
+          res.render('publishers', { publishers: results[0]});
+      } else {
+          // If no product with the given ID was found, render a 404 page or handle it accordingly
+          res.status(404).send('Publisher not found');
+      }
+  });
+});
+
+
+
 
 app.get('/login', (req, res) => {
     res.render('login', { messages: req.flash('success'), errors: req.flash('error') });
 });
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
 
-    // Validate email and password
-    if (!email || !password) {
-        req.flash('error', 'All fields are required.');
-        return res.redirect('/login');
-    }
 
-    const sql = 'SELECT * FROM users WHERE email = ? AND password = SHA1(?)';
-    connection.query(sql, [email, password], (err, results) => {
-        if (err) {
-            throw err;
-        }
 
-        if (results.length > 0) {
-            // Successful login
-            req.session.user = results[0]; 
-            req.flash('success', 'Login successful!');
-            if(req.session.user.role == 'user')
-                res.redirect('/shopping');
-            else
-                res.redirect('/inventory');
-        } else {
-            // Invalid credentials
-            req.flash('error', 'Invalid email or password.');
-            res.redirect('/login');
-        }
-    });
+app.get('/addPublisher', (req, res) => {
+    res.render('addPublisher');
 });
 
-app.get('/shopping', checkAuthenticated, (req, res) => {
-    // Fetch data from MySQL
-    connection.query('SELECT * FROM products', (error, results) => {
-        if (error) throw error;
-        res.render('shopping', { user: req.session.user, products: results });
-      });
-});
-
-app.post('/add-to-cart/:id', checkAuthenticated, (req, res) => {
-    const productId = parseInt(req.params.id);
-    const quantity = parseInt(req.body.quantity) || 1;
-
-    connection.query('SELECT * FROM products WHERE productId = ?', [productId], (error, results) => {
-        if (error) throw error;
-
-        if (results.length > 0) {
-            const product = results[0];
-
-            // Initialize cart in session if not exists
-            if (!req.session.cart) {
-                req.session.cart = [];
-            }
-
-            // Check if product already in cart
-            const existingItem = req.session.cart.find(item => item.productId === productId);
-            if (existingItem) {
-                existingItem.quantity += quantity;
-            } else {
-                req.session.cart.push({
-                    productId: product.productId,
-                    productName: product.productName,
-                    price: product.price,
-                    quantity: quantity,
-                    image: product.image
-                });
-            }
-
-            res.redirect('/cart');
-        } else {
-            res.status(404).send("Product not found");
-        }
-    });
-});
-
-app.get('/cart', checkAuthenticated, (req, res) => {
-    const cart = req.session.cart || [];
-    res.render('cart', { cart, user: req.session.user });
-});
-
-app.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
-});
-
-app.get('/product/:id', checkAuthenticated, (req, res) => {
-  // Extract the product ID from the request parameters
-  const productId = req.params.id;
-
-  // Fetch data from MySQL based on the product ID
-  connection.query('SELECT * FROM products WHERE productId = ?', [productId], (error, results) => {
-      if (error) throw error;
-
-      // Check if any product with the given ID was found
-      if (results.length > 0) {
-          // Render HTML page with the product data
-          res.render('product', { product: results[0], user: req.session.user  });
-      } else {
-          // If no product with the given ID was found, render a 404 page or handle it accordingly
-          res.status(404).send('Product not found');
-      }
-  });
-});
-
-app.get('/addProduct', checkAuthenticated, checkAdmin, (req, res) => {
-    res.render('addProduct', {user: req.session.user } ); 
-});
-
-app.post('/addProduct', upload.single('image'),  (req, res) => {
+app.post('/addPublisher', upload.single('images'),  (req, res) => {
     // Extract product data from the request body
-    const { name, quantity, price} = req.body;
-    let image;
+    const { publisher_name, publisher_address, publisher_country, publisher_contact} = req.body;
+    let images;
     if (req.file) {
-        image = req.file.filename; // Save only the filename
+        images = req.file.filename; // Save only the filename
     } else {
-        image = null;
+        images = "noImage.png";
     }
 
-    const sql = 'INSERT INTO products (productName, quantity, price, image) VALUES (?, ?, ?, ?)';
+    const sql = 'INSERT INTO publishers (publisher_name, publisher_address, publisher_country, publisher_contact, images) VALUES (?, ?, ?, ?, ?)';
     // Insert the new product into the database
-    connection.query(sql , [name, quantity, price, image], (error, results) => {
+    connection.query(sql , [publisher_name, publisher_address, publisher_country, publisher_contact, images], (error, results) => {
         if (error) {
             // Handle any error that occurs during the database operation
-            console.error("Error adding product:", error);
-            res.status(500).send('Error adding product');
+            console.error("Error adding publisher:", error);
+            res.status(500).send('Error adding publisher');
         } else {
             // Send a success response
-            res.redirect('/inventory');
+            res.redirect('/');
         }
     });
 });
 
-app.get('/updateProduct/:id',checkAuthenticated, checkAdmin, (req,res) => {
-    const productId = req.params.id;
-    const sql = 'SELECT * FROM products WHERE productId = ?';
+app.get('/editPublisher/:id', (req,res) => {
+    const publisher_id = req.params.id;
+    const sql = 'SELECT * FROM publishers WHERE publisher_id = ?'; 
 
-    // Fetch data from MySQL based on the product ID
-    connection.query(sql , [productId], (error, results) => {
-        if (error) throw error;
-
-        // Check if any product with the given ID was found
-        if (results.length > 0) {
-            // Render HTML page with the product data
-            res.render('updateProduct', { product: results[0] });
+    connection.query(sql, [publisher_id], (error, results) => { 
+        if (error) { 
+            console.error('Database query error:', error.message);
+            return res.status(500).send('Error Retrieving publisher by ID');
+            
+        }
+        
+        if (results.length > 0) { 
+            res.render('updatePublisher', {product: results[0]}); 
         } else {
-            // If no product with the given ID was found, render a 404 page or handle it accordingly
-            res.status(404).send('Product not found');
-        }
+            
+           res.status(404).send('Publisher not found');
+    }
     });
 });
 
-app.post('/updateProduct/:id', upload.single('image'), (req, res) => {
-    const productId = req.params.id;
-    // Extract product data from the request body
-    const { name, quantity, price } = req.body;
-    let image  = req.body.currentImage; //retrieve current image filename
-    if (req.file) { //if new image is uploaded
-        image = req.file.filename; // set image to be new image filename
+app.post('/editPublisher/:id', upload.single('images'), (req, res) => {
+    const publisher_id = req.params.id;
+    const {publisher_name,publisher_address,publisher_country,publisher_contact} = req.body;
+    let images  = req.body.currentImages; 
+    if (req.file) { 
+        images = req.file.filename; 
     } 
 
-    const sql = 'UPDATE products SET productName = ? , quantity = ?, price = ?, image =? WHERE productId = ?';
+    const sql = 'UPDATE publishers SET publisher_name = ? , publisher_address = ?, publisher_country = ?, publisher_contact =?, images =? WHERE publisher_id = ?';
     // Insert the new product into the database
-    connection.query(sql, [name, quantity, price, image, productId], (error, results) => {
+    connection.query(sql, [publisher_name, publisher_address, publisher_country, publisher_contact, images, publisher_id], (error, results) => {
         if (error) {
             // Handle any error that occurs during the database operation
-            console.error("Error updating product:", error);
-            res.status(500).send('Error updating product');
+            console.error("Error updating publisher:", error);
+            res.status(500).send('Error updating publisher');
         } else {
             // Send a success response
-            res.redirect('/inventory');
+            res.redirect('/');
         }
     });
 });
 
-app.get('/deleteProduct/:id', (req, res) => {
-    const productId = req.params.id;
-
-    connection.query('DELETE FROM products WHERE productId = ?', [productId], (error, results) => {
+//Delete route//
+app.get('/deletePublisher/:id',(req,res) => {
+    const publisher_id = req.params.id;
+    //Extract product data from the request body
+    const sql = 'DELETE FROM publishers WHERE publisher_id = ?' ;
+    //Insert the new product into the database: connection object to talk to db
+    connection.query(sql, [publisher_id], (error,results) => { //These 4 info is to be passed to SQL statement, which is why there are 4 question marks//
         if (error) {
-            // Handle any error that occurs during the database operation
-            console.error("Error deleting product:", error);
-            res.status(500).send('Error deleting product');
+            //Handle any error that occurs during the database operation//
+            console.error("Error deleting publisher:", error);
+            res.status(500).send('Error deleting publisher');
+
         } else {
-            // Send a success response
-            res.redirect('/inventory');
+            //Send a success response
+            res.redirect('/');
         }
     });
+
 });
+
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on URL address : http://localhost:${PORT}/`));
+
